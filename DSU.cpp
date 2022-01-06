f2052a93a80db55b24763db4547def5bee2a0592
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-06 17:48:03 +0530

    Create DSU.cpp

---
+++ DSU.cpp
@@ -0,0 +1,21 @@
+ void make(int v){
+     parent[v]=v;
+     sz[v]=1;
+ }   
+
+ int find(int v){
+     if(v==parent[v]) return v;
+     return parent[v]=find(parent[v]);
+ }
+    
+void Union(int u,int v){
+u=find(u);
+v=find(v);
+    if(u!=v){
+        if(sz[u]<sz[v]) swap(u,v);
+        parent[v]=u;
+        sz[u]+=sz[v];
+    }
+}    
+    
+  
