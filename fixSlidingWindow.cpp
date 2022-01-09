078d430d46e6c2b8dc15a64e215c067a34910e80
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-09 10:52:44 +0530

    Create fixSlidingWindow.cpp

---
+++ fixSlidingWindow.cpp
@@ -0,0 +1,17 @@
+int start=0,end=0;
+while(end<n){
+//Each Step[index] Calculation
+if(end-start+1<k){
+//Move end Pointer
+}
+//Maintain this window Size.
+else if(end-start+1==k){
+//Get your final Answer by concluding this window and compare with previous answer
+
+//Exclude some feasible answer from start pointer if this answer is part of current window Answer
+  
+//Move start pointer
+ 
+//Move End Pointer
+
+}
