36f0b4bc04608e0a2e67370f965ba7584de4d19f
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-10 21:13:43 +0530

    Update topologicalSorting_bfs.cpp

--- topologicalSorting_bfs.cpp
+++ topologicalSorting_bfs.cpp
@@ -1,1 +1,38 @@
 
+void topological_bfs() {
+
+	for (int i = 1; i <= V; i++) {
+		for (auto j : adj[i]) {
+			indegree[j]++;
+		}
+	}
+
+//take priority_queue or queue For correct order you can take pq
+	priority_queue<int,  vector<int>, greater<int>> pq;
+	int cnt = 0;//
+	for (int i = 1; i <= V; i++) {
+		if (indegree[i] == 0) {
+			debug(i);
+			pq.push(i);
+		}
+	}
+
+	while (!pq.empty()) {
+		int u = pq.top();
+		pq.pop();
+		topo.push_back(u);
+		for (int v : adj[u]) {
+			if (--indegree[v] == 0) {
+				pq.push(v);
+			}
+			cnt++;//sari edges ko traverse kar rhe ho ya nhi
+		}
+	}
+	if (cnt != E) {
+		cout << "Cycles is Present";
+	} else {
+		for (int i : topo) {
+			cout << i << " ";
+		}
+	}
+}
