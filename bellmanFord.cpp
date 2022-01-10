d4b4e5b405df13fff92a28383e12247a81befb7f
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-10 11:13:30 +0530

    Update bellmanFord.cpp

--- bellmanFord.cpp
+++ bellmanFord.cpp
@@ -1,1 +1,26 @@
-
+//Variables have their usual Meaning 
+void bellmanFord(int source) {
+	int x;
+	for (int i = 0; i < V; i++) {
+		x = -1;
+		for (int j = 0; j < E; j++) {
+			int u = edges[j][0];
+			int v = edges[j][1];
+			int wt = edges[j][2];
+			if (dis[u] < INF) {
+				if (dis[v] > dis[u] + wt) {
+					//Why we are comparing with -INF because
+					//distance can be -ve can be cross overflow
+					dis[v] = max(-INF, dis[u] + wt);
+					parent[v] = u;
+					x = v;
+				}
+			}
+		}
+	}
+	if (x == -1) {
+		cout << "NO Negative CYCLE IS PRESENT" << endl;
+	} else {
+		cout << "Negative Cycle is Preseent";
+	}
+}
