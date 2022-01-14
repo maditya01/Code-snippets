23f3f212bd8ec16363b9c618c984ea165e9d02c6
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-14 21:59:33 +0530

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,14 +1,15 @@
 //Remeber which data structure i am using dis-> distance array of size n or n+1 
 //ms-> multiset for getting minimum distance, pair of {distance,vertex}-> first distance second is vertex 
 //edges-> graph representation using list
+//Take a graph and visualise this algo at every step.
 void shortestPath(int source){
      dis[source]=0;
      ms.insert({0,source});
      while(!ms.empty()){
+         int d=ms.begin()->first;//Try to Get Minimum Distance
          int u=ms.begin()->second;
-          int d=ms.begin()->first;
          ms.erase(ms.begin());
-          if(d>dis[u]) continue;
+          if(d>dis[u]) continue;//if d is greater that dis[u] 
          for(auto v:edges[u]){
              if(dis[v.first]> dis[u]+v.second){
                  dis[v.first]=dis[u]+v.second;
