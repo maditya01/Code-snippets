516de1f30efb5fb878b2f2dfd18f9d963c247cd0
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-17 12:36:42 +0530

    Update recursiveDP.cpp

--- recursiveDP.cpp
+++ recursiveDP.cpp
@@ -1,1 +1,16 @@
 //Code How to Think in terms of DP.
+//We have 2 types of recursion Parametrised Recursion,Functional Recursion 
+//In DP we mostly used Functional Recursion
+Take a Data_Structure for storing DP value initialise with -1;
+return_type recursive_DP(input arguments){
+//Base Case:
+
+//DP condition Check
+
+
+//Find out all The choices
+i) 2 choices take or not_take
+ii) n choices use For loop
+
+return best Posible answer from these Choices.
+}
