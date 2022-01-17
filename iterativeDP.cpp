b3d4163b4f422ec26658592568e1252e71fc5b37
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-17 12:54:56 +0530

    Update iterativeDP.cpp

--- iterativeDP.cpp
+++ iterativeDP.cpp
@@ -1,2 +1,12 @@
 // How to Think in terms of iterative
 // How to Think DP problem in Terms of State and Transition
+
+Here we are thinking in term of DP state
+what dp[i]-> telling ,what dp[i][j]-> telling ...etc 
+
+What is the Transition from one DP state to another
+dp[i]=dp[i-x]+something like that dp[i][j]= dp[i-1][j]+ dp[i-1][j-nums[i-1]] something like that
+
+
+what is the answer of smallest DP state which you can directly tell  like dp[0]=something dp[n] =something dp[0][0]=something 
+One question comes when you write Iterative DP where you have to start-> then your smallest DP state would be helpFull.
