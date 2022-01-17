b197cfc2b7894a28c96abdd531aec4fe4f456353
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-17 12:39:58 +0530

    Update recursiveDP.cpp

--- recursiveDP.cpp
+++ recursiveDP.cpp
@@ -1,9 +1,14 @@
+//Here we are not thinking in term of DP state
+//Here we are more concern about how to find choices and how to write recursion and recurrance relation
+
 //Code How to Think in terms of DP.
 //We have 2 types of recursion Parametrised Recursion,Functional Recursion 
 //In DP we mostly used Functional Recursion
 Take a Data_Structure for storing DP value initialise with -1;
 return_type recursive_DP(input arguments){
+
 //Base Case:
+ return answer for base Case.
 
 //DP condition Check
 
@@ -12,5 +17,5 @@ return_type recursive_DP(input arguments){
 i) 2 choices take or not_take
 ii) n choices use For loop
 
-return best Posible answer from these Choices.
+return [stored in dp] = best Posible answer from these Choices.
 }
