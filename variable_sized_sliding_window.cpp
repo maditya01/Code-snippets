65aa35d38737f68868625759d75dc80538b4b411
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-21 19:34:27 +0530

    Create variable_sized_sliding_window.cpp

---
+++ variable_sized_sliding_window.cpp
@@ -0,0 +1,23 @@
+//Window size is variable
+
+int i=0,j=0;
+
+while(j<size){
+//some calculation for index j.
+if(cond<k){
+   j++;   
+}
+else if(cond==k){
+//Calculate the answer
+  j++;
+}
+else if(cond>k){
+  while(cond>k){
+    remove calculation of i
+      i++;
+  }
+    j++;
+}
+
+
+}
