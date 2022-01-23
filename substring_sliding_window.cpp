67d44cc443547eb7ff904c169f00677c5759917a
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-23 18:42:20 +0530

    Create substring_sliding_window.cpp

---
+++ substring_sliding_window.cpp
@@ -0,0 +1,30 @@
+    vector<int> map(128, 0);//What is the size of map according to string letters 
+
+		 int counter; // check whether the substring is valid Given Condition ko count karne ke liye 
+    //like how many unique characters are present how many repeating characters are present in that window_substring 
+
+
+		int begin = 0, end = 0; //two pointers, one point to tail and one  head -> valid window ke start,end honge at the end
+
+		int len; //the length of substring
+
+//Initialisation Some where Needed Some where Not Needed 
+		for () { /* initialize the hash map here */ }
+
+		while (end < s.size()) {
+    //if you have Initialize before then apply --, otherwise apply ++
+     if (map[s[end]]-- ? ++ ) {  /* modify counter here */ }
+      
+      //Your Current Window is Going to Invalidate Given Condition after here then apply this condition 
+			while (/* counter condition */) {
+				/* update len here if finding minimum*/
+        
+				//increase begin to make it invalid/valid again
+				if (map[s[begin]]++ ? -- ) { /*modify counter here*/ }
+        begin++;
+			}
+
+			/* update len here if finding maximum*/
+      end++;
+		}
+		return d;
