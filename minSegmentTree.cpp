211c4e606db782963d602aa543a1aadef7ebe4e5
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-26 09:28:50 +0530

    Update minSegmentTree.cpp

--- minSegmentTree.cpp
+++ minSegmentTree.cpp
@@ -2,7 +2,7 @@ vector<int> segTree;
 
 
 //initialise the segment Tree.
-void minSegConstruct(vector<int> &arr) {
+void generalSegConstruct(vector<int> &arr) {
 	int n = arr.size();
 	//segment tree of size 2*n
 	segTree.resize(2 * n);
@@ -14,7 +14,7 @@ void minSegConstruct(vector<int> &arr) {
 //create upper level of tree using last level
 	for (int i = n - 1; i >= 0; i--) {
 		//left child and right child
-		segTree[i] = min(segTree[2 * i], segTree[2 * i + 1]);
+		segTree[i] = what you want(segTree[2 * i] OPERATOR segTree[2 * i + 1]);
 	}
 }
 
@@ -32,32 +32,31 @@ void update(int idx, int value) {
 //idx=1 for root
 	while (idx > 1) {
 		int parent = idx / 2;
-		segTree[parent] = min(segTree[2 * parent], segTree[2 * parent + 1]);
+		segTree[parent] = what you want(segTree[2 * parent] OPERATOR segTree[2 * parent + 1]);
 		idx = idx / 2;
 	}
-	debug(segTree);
 }
 
 
 
-int minimumRange(int l, int r) {
+int generalRange(int l, int r) {
 //find the minimum value in the range from l to r
 //l is included an r is not included
 //if your r is included then do r=r+1.
 	int n = segTree.size() / 2;
 	l = l + n;
 	r = r + n;
-	int mn = INT_MAX;
+	int mn = what you want;
 
 	while (l < r) {
 		if (l % 2) {
-			mn = min(mn, segTree[l]);
+			mn = what you want(mn OPERATOR segTree[l]);
 			l = l + 1;
 		}
 		//r is not included in the range
 		if (r % 2) {
 			r = r - 1;
-			mn = min(mn, segTree[r]);
+			mn = what you want (mn OPERATOR segTree[r]);
 		}
 		l = l / 2;
 		r = r / 2;
