7638213f780124495601d5b72a2b7c1f0ac08872
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-26 17:09:25 +0530

    Update binaryIndextree.cpp

--- binaryIndextree.cpp
+++ binaryIndextree.cpp
@@ -1,10 +1,14 @@
 int n;
 vector<int> bitTree;
 //return the sum from index-> 1 to idx.
+//Given index kis-kis range se milkar bana hai
+//For example idx= 13-> [13,13] idx= 12->[9,12] idx=8->[1,8] idx=0
+
 int sum(int idx) {
 	int res = 0;
 	while (idx > 0) {
 		res += (bitTree[idx]);
+		//Extract the last set bit from index
 		idx = idx - ((idx) & (-idx));
 	}
 	return res;
@@ -13,7 +17,9 @@ int sum(int idx) {
 void update(int idx, int value) {
 	while (idx <= n) {
 		bitTree[idx] += value; //add given value.
-		idx += (idx) & (-idx);
+		Add the last set bit to the index
+		idx += (idx) & (-idx);//Given idx kha kha contribute karega
+// 		[1-8] each element from 1-8 is contributing in to 8th index
 	}
 }
 
