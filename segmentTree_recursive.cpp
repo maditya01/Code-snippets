393cf8d8e9e9803fd35b8a1670dd2e06bb4ab8e6
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-30 11:58:01 +0530

    Update segmentTree_recursive.cpp

--- segmentTree_recursive.cpp
+++ segmentTree_recursive.cpp
@@ -4,15 +4,15 @@
 const int MAXN = int(1e5) + 5;
 int n, tree[4 * MAXN];
 //BUILD OPERATION
-void build(vector<int> &nums, int pos, int start, int end) {
+void build(vector<int> &nums, int node, int start, int end) {
 	if (start == end) {
-		tree[pos] = nums[start];
+		tree[node] = nums[start];
 		return;
 }
 	int mid = (start + end) / 2;
-	build(nums, pos * 2, start, mid);
-	build(nums, pos * 2 + 1, mid + 1, end);
-	tree[pos] = tree[pos * 2] + tree[pos * 2 + 1];
+	build(nums, node * 2, start, mid);
+	build(nums, node * 2 + 1, mid + 1, end);
+	tree[node] = tree[node * 2] + tree[node * 2 + 1];
 }
 
 //QUERY OPERATION
@@ -22,7 +22,7 @@ void build(vector<int> &nums, int pos, int start, int end) {
 // our range got partially overlap with node range[1, 4] or [3, 6];-> aur depth me jayenge
 // our range does not overlap with that node[0, 1] or [6, 6] -> infinite return karenga
 
-int query(int l, int r, int start, int end, int pos) {
+int query(int l, int r, int start, int end, int node) {
 //node ke range kaise nikalenge
 
 	//does not overlap
@@ -31,12 +31,12 @@ int query(int l, int r, int start, int end, int pos) {
 	}
 
 	//completely overlap
-	if (start >= l and end <= r) return tree[pos];
+	if (start >= l and end <= r) return tree[node];
 
 
 	//partially overlap-> aur depth me jao
 	int mid = (start + end) / 2;
-	return (query(l, r, start, mid, 2 * pos) + query(l, r, mid + 1, end, 2 * pos + 1));
+	return (query(l, r, start, mid, 2 * node) + query(l, r, mid + 1, end, 2 * node + 1));
 
 }
 
@@ -44,20 +44,20 @@ int query(int l, int r, int start, int end, int pos) {
 //UPDATE OPERATION
 // update(1,0,n-1,pos,new_val)
 //pos is a index where we have to update 
-void update(int ind, int start, int end, int pos, int new_val) {
+void update(int node, int start, int end, int pos, int new_val) {
 	//Leaf Node
 	if (start == end) {
-		tree[ind] = new_val;
+		tree[node] = new_val;
 		return;
 	}
 
 	int mid = (start + end) / 2;
-//left child
-	if (pos <= mid) update(ind * 2, start, mid, pos, new_val);
+//pos is less than mid ->left child
+	if (pos <= mid) update(node * 2, start, mid, pos, new_val);
 //right child
-	else update(ind * 2 + 1, mid + 1, end, pos, new_val);
+	else update(node * 2 + 1, mid + 1, end, pos, new_val);
 
-	tree[ind] = tree[ind * 2] + tree[ind * 2 + 1];
+	tree[node] = tree[node * 2] + tree[node * 2 + 1];
 }
 
 
