93075b7003dec67109d615a9260948cbdbf10c31
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-01-31 21:38:26 +0530

    Update topologicalSorting_dfs.cpp

--- topologicalSorting_dfs.cpp
+++ topologicalSorting_dfs.cpp
@@ -1,1 +1,11 @@
-
+bool visited[N];
+void dfs(int source) {
+	visited[source] = true;
+	for (auto v : adj[source]) {
+		if (!visited[v]) {
+			dfs(v);
+		}
+	}
+  //u is Coming as topo Order 
+	ans.push_back(source);
+}
