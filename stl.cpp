066d231c978c1122be629aacd5c2c7729261f4b9
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-03-09 09:55:52 +0530

    Create stl.cpp

---
+++ stl.cpp
@@ -0,0 +1,27 @@
+ARRAY,VECTOR
+
+LIST
+
+STACK,QUEUE
+
+SET,MULTISET
+
+// MAP,UNORDEREDMAP
+map<int,int> m; // normal jab (key,value) need ho key se sorted hoga increasing order me
+
+
+
+map<int,vector<int>> m; //multimap use karne se aacha hai ye kar lo ak key ke corresponding vector aayega.
+
+
+map<int,map<int,int>> m; //{a,b,c} first a ke hissab se sort ho a same ho to b ke hissab se sort ho aur Frequency Store karni ho.
+
+
+
+map<int,int> m[size]; //at each index you have to mainTain a freq of this thing  [index][someVariable] a 2d matrix.
+
+SORTING ALGO
+
+
+
+METHODS:
