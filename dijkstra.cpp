1e490a8910ac25582ed58c3e8fb237eb2e7b0e3c
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-03-09 09:38:16 +0530

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -3,6 +3,9 @@
 //ms-> multiset for getting minimum distance, pair of {distance,vertex}-> first distance second is vertex 
 //edges-> graph representation using list
 //Take a graph and visualise this algo at every step.
+
+
+//distance is an array . where index represents vertex and value represents min distance from source vertex.
 void shortestPath(int source){
      dis[source]=0;//source has distance 0 from source itself
      ms.insert({0,source});//insert that thing in ms
@@ -10,10 +13,13 @@ void shortestPath(int source){
          int d=ms.begin()->first;//Try to get a vertex which has a Minimum Distance-> GREEDY APPROACH
          int u=ms.begin()->second;//vertex associated with that distance
          ms.erase(ms.begin());
+          
+          //By Another Path dis[u] is Minimised.
           if(d>dis[u]) continue;//if d is greater that dis[u] 
+          
          for(auto v:edges[u]){
              if(dis[v.first]> dis[u]+v.second){
-                 dis[v.first]=dis[u]+v.second;
+                 dis[v.first]= dis[u]+v.second;
                  ms.insert({dis[v.first],v.first});
              }
          }
@@ -22,3 +28,4 @@ void shortestPath(int source){
 
 //Now you have a distance array which has distance from source to ith vertex ->
 // graph 1 based hai ya 0 based  usi ke according distance array hai
+//if 1 based size(N+1) 0 based hai Size N hoga
