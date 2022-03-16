2c7987797e4db17f846af1bf7ed0da96da754616
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-03-16 16:10:37 +0530

    Update graph_structure.cpp

--- graph_structure.cpp
+++ graph_structure.cpp
@@ -1,1 +1,26 @@
 
+//For Writing in interview.
+
+struct Edge {
+	int u, v, wt;
+};
+
+
+struct Graph {
+
+//No of vertexes and No of Edges
+	int V, E;
+//List of Edges.
+	struct Edge* edges;
+}
+
+// Creates a graph with V vertices and E edges
+
+struct Graph* createGraph(int V, int E) {
+
+	struct Graph* graph = new Graph;
+	graph->V = V;
+	graph->E = E;
+	graph->edge = new Edge[E];
+	return graph;
+}
