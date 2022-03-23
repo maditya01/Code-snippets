39be4be2558fc6d3efa8cf968537ce2ca7223c85
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-03-23 18:30:11 +0530

    Update segmentTree_recursive.cpp

--- segmentTree_recursive.cpp
+++ segmentTree_recursive.cpp
@@ -1,4 +1,4 @@
-//  i am writing sum for segment tree
+//->I am writing sum for segment tree
 
 
 const int MAXN = int(1e5) + 5;
@@ -12,7 +12,7 @@ void build(vector<int> &nums, int node, int start, int end) {
 	int mid = (start + end) / 2;
 	build(nums, node * 2, start, mid);
 	build(nums, node * 2 + 1, mid + 1, end);
-	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	tree[node] = tree[node * 2] [operator what you wannt] tree[node * 2 + 1];
 }
 
 //QUERY OPERATION
@@ -20,15 +20,16 @@ void build(vector<int> &nums, int node, int start, int end) {
 //query wala 3 case-> range->[2,5]
 // our range got completely overlap or inside with node range [2, 5] or [2,3] or [3,4] return karenge
 // our range got partially overlap with node range[1, 4] or [3, 6];-> aur depth me jayenge
-// our range does not overlap with that node[0, 1] or [6, 6] -> infinite return karenga
+// our range does not overlap with that node[0, 1] or [6, 6] -> infinite return karenge
 
 //l---r is a range where we have to query
-//start----end is a range of current node we have to compare
-//with l---r range
+
+//start----end is a range of current node we have to compare with l---r range
 int query(int l, int r, int start, int end, int node) {
 //node ke range kaise nikalenge
 
-	//does not overlap
+	//no overlap
+	//Out of left Side hai.  //Out of Right Side hai 
 	if ((start < l and end < l) or (start > r and end > r)) {
 		return 0;
 	}
@@ -39,7 +40,7 @@ int query(int l, int r, int start, int end, int node) {
 
 	//partially overlap-> aur depth me jao
 	int mid = (start + end) / 2;
-	return (query(l, r, start, mid, 2 * node) + query(l, r, mid + 1, end, 2 * node + 1));
+	return (query(l, r, start, mid, 2 * node) [operator what you wannt] query(l, r, mid + 1, end, 2 * node + 1));
 
 }
 
@@ -47,6 +48,7 @@ int query(int l, int r, int start, int end, int node) {
 //UPDATE OPERATION
 // update(1,0,n-1,pos,new_val)
 //pos is a index where we have to update 
+//left Subtree me jana hai ya right subtree me jana hai.
 void update(int node, int start, int end, int pos, int new_val) {
 	//Leaf Node
 	if (start == end) {
@@ -60,7 +62,7 @@ void update(int node, int start, int end, int pos, int new_val) {
 //right child
 	else update(node * 2 + 1, mid + 1, end, pos, new_val);
 
-	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	tree[node] = tree[node * 2] [operator what you wannt] tree[node * 2 + 1];
 }
 
 
