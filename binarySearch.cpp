4a659063bca65db0721f340c3c92eab248d02a61
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-04-03 09:43:46 +0530

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,10 +1,18 @@
 bool check(input_arguments){
+ 
+ //Each Time This Function will Change
+ 
  //input_arguments me minimum value ya maximum value hi ayegi
 //Time complexity o(logN) or you can do  o(N) according to question
 }
 
 
 int lo= lowest possible value in range, hi=  highest possible value in range,ans;
+
+//The most important thing BS lagana kis range par hai.
+// kis attribute ko range me lena hai.isi par dimaak lagana hai
+
+
 //TC-> O(logN)*O(checkFunction)
 while(lo<=hi){
 int mid= lo+(hi-lo)/2;
