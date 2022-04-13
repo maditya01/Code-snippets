2a6bfd03c710ac2eb97eb36b5e0ccfc6ae9aeba9
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-04-13 17:01:09 +0530

    Update primeFactorization.cpp

--- primeFactorization.cpp
+++ primeFactorization.cpp
@@ -2,14 +2,17 @@ vector<int> pf;//Stores all the prime factors of a number
 //O(sqrt(N))
 void primeFactor(int N){
  for(int i=2; i*i <=N;i++){
+     if(N%i==0){
+    pf.push_back(i);//only Single prime factors
      while(N%i==0){
-       pf.push_back(i);
+      //All prime factors.
        N=N/i;
      }
   }
 //i*i-> works only for Non Prime Numbers 
+//if after dividing at any step our number is prime so we can not get any pf <sqrt(i)
 if(n>1){
-    pf.push_back(i);
+    pf.push_back(n);
 }
 }
 
