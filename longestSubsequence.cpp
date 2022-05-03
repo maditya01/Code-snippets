5fd094f66c4e803e5adf5456feb3de4ddfeba1bc
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-05-03 12:39:08 +0530

    Create longestSubsequence.cpp

---
+++ longestSubsequence.cpp
@@ -0,0 +1,12 @@
+//Longest Subsequence with some property.
+// Subsequence ends at ith index.
+ //Base case -> each element is itself longest with ending at itself.
+// what dp state define 
+//One-d DP 2-d Dp may be with some constraint 
+for(int i=1;i<n;i++){
+ 
+  for(int ind=i-1;ind>=0;ind--){
+  //apply some property.
+  }
+  
+}
