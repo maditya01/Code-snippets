7208c0a052cb087e2ac132b049a7e8a7a380f28d
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-05-03 12:32:07 +0530

    Create countnoofSubsequence.cpp

---
+++ countnoofSubsequence.cpp
@@ -0,0 +1,18 @@
+//Count Number of Subsequences of legth k with some property.
+
+//subsequence ends at ith index ka concept lagega.
+
+
+//length = 1 is your base case. for all index.
+
+for(int i=1;i<n;i++){
+ 
+  for(int len=2;len<=k;len++){
+    
+    for(int ind=i-1;ind>=0;ind--){
+      //according to property check nums[i] with nums[ind] and take with len-1 prev DP state
+      
+    }
+    
+  }
+}
