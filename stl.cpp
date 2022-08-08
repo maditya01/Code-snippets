628480db3446004fa50dc47819eed57f9c346b1f
aditamaurya <2082001aditya@gmail.com>
2022-08-08 12:19:15 +0530

    Changes Made

--- stl.cpp
+++ stl.cpp
@@ -1,24 +1,24 @@
-ARRAY,VECTOR
+ARRAY, VECTOR
 
 LIST
 
-STACK,QUEUE
+STACK, QUEUE
 
-SET,MULTISET
+SET, MULTISET
 
 // MAP,UNORDEREDMAP
-map<int,int> m; // normal jab (key,value) need ho key se sorted hoga increasing order me
+map<int, int> m; // normal jab (key,value) need ho key se sorted hoga increasing order me
 
 
 
-map<int,vector<int>> m; //multimap use karne se aacha hai ye kar lo ak key ke corresponding vector aayega.
+map<int, vector<int>> m; //multimap use karne se aacha hai ye kar lo ak key ke corresponding vector aayega.
 
 
-map<int,map<int,int>> m; //{a,b,c} first a ke hissab se sort ho a same ho to b ke hissab se sort ho aur Frequency Store karni ho.
+map<int, map<int, int>> m; //{a,b,c} first a ke hissab se sort ho a same ho to b ke hissab se sort ho aur Frequency Store karni ho.
 
 
 
-map<int,int> m[size]; //at each index you have to mainTain a freq of this thing  [index][someVariable] a 2d matrix.
+map<int, int> m[size]; //at each index you have to mainTain a freq of this thing  [index][someVariable] a 2d matrix.
 
 SORTING ALGO
 
