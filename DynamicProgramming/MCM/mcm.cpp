0f9dc5d2777dab638dc6c87feb5fcef989aef8e7
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2022-08-16 22:35:56 +0530

    Update mcm.cpp

--- DynamicProgramming/MCM/mcm.cpp
+++ DynamicProgramming/MCM/mcm.cpp
@@ -1,3 +1,5 @@
+// How to Start think in to this type of problem start to multiply one by one take some segment of given array try to multiply
+
 //[.  .  .  .  .]
 //by taking differnt Number of elements [. .]*[. . .] 
 //[.]*[. . . .]
@@ -5,6 +7,10 @@
  //Here in Matrix Chain Multiplication we are doing same thing we try to multiply matrix in different manners.
 //Cost of 2  Matrix to multiply is  i*j j*k  => i*j*k.
 
+[10,20,30,40,50] (-> n=5.)
+My Dp Table is of 4*4 means (n-1)*(n-1). because total Number of matrix is 4.
+//i have to tell how to multiply these Matrixes so that minCost is Possible.
+ 
 int matrixMultiplication(vector<int> &arr, int N){
    
     int n = N;
