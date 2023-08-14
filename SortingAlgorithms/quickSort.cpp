0e2ca4e9330d555c2e89c46b7ffd7815ef6182ca
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2023-08-14 10:35:27 +0530

    Update quickSort.cpp

--- SortingAlgorithms/quickSort.cpp
+++ SortingAlgorithms/quickSort.cpp
@@ -1,3 +1,9 @@
+Analysis of quick sort algorithms-> Quick sort is a divide and conquer algorithm.
+Average Case: T(n) = T(n/10) + T(9n/10) + O(N) = O(NlogN)
+Best Case: 
+Worst Case: if we choose the maximum or minimum element as the pivot element then T.C = T(n) = T(n-1) + O(N) = O(N^2)
+
+
 int partition (int arr[], int low, int high){
        int X=arr[low];//low-> Pivot Point Man rhe hain
       //At the End before X all the values are less than X After X all the Values are greater than X.
