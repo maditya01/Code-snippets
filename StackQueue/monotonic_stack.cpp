f4cbada0fc6092fddbabda9813d7d885d002dfad
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2023-08-15 09:52:53 +0530

    Create monotonic_stack.cpp

---
+++ StackQueue/monotonic_stack.cpp
@@ -0,0 +1,12 @@
+What could be the template for stack.
+iterate for loop from 0:n-1{ 
+while(pop ke condition when you want to pop){}
+push->s[i]
+}
+just iterate stack and push answer in ans. reverse ans 
+return ans.
+
+  //I have to create incresing stack, I have to create decreasing stack.
+  //I have to tell next greater element for each element of an array.
+  //We have 4 options NGL(next greater left side), NGR(next greater right side), NSL(next smallest Left), NSR(next smallest right side)
+  
