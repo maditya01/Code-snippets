d87a959d0558cb8415fcfd7c6022a387e4ada092
Aditya Maurya <60269271+maditya01@users.noreply.github.com>
2023-08-19 16:06:53 +0530

    Update selectionSort.cpp

--- SortingAlgorithms/selectionSort.cpp
+++ SortingAlgorithms/selectionSort.cpp
@@ -1,11 +1,15 @@
-//First select index i then loop from  i+1 to n to find minimum value index and then swap that index with index i.
-//Left part already sorted
+//First select index i then loop from  i+1 to n to find the minimum value index and then swap that index with index i.
+//Left part is already sorted
 /* Advantages of Selection Sort Algorithm
    Simple and easy to understand.
    Works well with small datasets.*/
 Does not work well on large datasets.
 Does not preserve the relative order of items with equal keys which means it is not stable.
 
+The worst-case time complexity of the Insertion sort is O(N^2)
+The average case time complexity of the Insertion sort is O(N^2)
+The time complexity of the best case is O(N^2).
+   
 int N;
     int select(int arr[], int i){
          int ind=i;
